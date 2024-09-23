3266d373283c70da6dfe1cabf758266a5ee8b066
kemalbarışerdem <kemalbariss123432@gmail.com>
2024-09-24 01:03:00 +0300

    24.09.2024 hiyerarşi ve bootstrap denemesi

---
+++ src/app/admin/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
